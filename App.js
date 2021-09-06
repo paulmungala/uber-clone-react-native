4a5b534029cc66075b61263ac172cc6aef6a90a6
paulmungala <mungalaaa8@gmail.com>
2021-09-06 13:05:24 +0300

    init first day

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
+// 1. Setting up redux.
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
   );
 }
 
